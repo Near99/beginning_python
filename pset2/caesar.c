86ff92892b05b43bfede66190670a7248af93ab2
Near99 <wkx0823@gmail.com>
2021-04-27 18:24:50 +0800

    Add files via upload

---
+++ pset2/caesar.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <cs50.h>
+/**
+ * Feels like a damn f* stupid when attempting this task
+ * It's quite similar to readability in terms of manipulating 
+ * strings with ASCII code. But I totaly forgot how I did 
+ * readability days ago, so was in trouble for a while XD.
+ * I wrote some piece code first that passed the check,
+ * It was, however, in chaos. This one still but slightly better
+ * as I referred to other coders work. I tried to use 
+ * -= += %= operatin format this time to clean up the code a bit.
+ * Gonna watch lecture 2 again cause I think I don't understand
+ * enough about the array. And will try to write a atoi function
+ * myself. Thanks to Federico-abss on github for your work :)
+ **/
+
+int main(int argc, string argv[])
+{
+    int k; //Rotating value
+    
+    if (argc != 2)
+    {
+        printf("False \n");
+        
+        return 1;
+    }
+    else if (atoi(argv[1]) <= 0)
+    {
+        printf("False \n");
+        
+        return 1;
+    }
+    else 
+    {
+        k = atoi(argv[1]);
+        
+        //Use the remainder as key value if > 26.
+        if (k > 26)
+        {
+            k %= 26;
+        }
+        
+        string plaintext = get_string("plaintext: ");
+        
+        for (int i = 0; i < strlen(plaintext); ++i)
+        {
+            //Uppercase letters
+            if (plaintext[i] >= 65 && plaintext[i] <= 90)
+            {
+                //Handle the out-of-range letters if plus key
+                if (plaintext[i] + k > 90)
+                {
+                    plaintext[i] -= 26;
+                }
+                plaintext[i] += k;
+            }
+            //Lowercase letters
+            else if (plaintext[i] >= 97 && plaintext[i] <= 122)
+            {
+                if (plaintext[i] + k > 122)
+                {
+                    plaintext[i] -= 26;
+                }
+                plaintext[i] += k;
+            }
+        }
+
+        printf("ciphertext: %s\n", plaintext);
+        
+        return 0;
+    }
+}
